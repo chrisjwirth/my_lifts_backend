4776d66023ea9ad6cdc5da916f03e731bacc8765
Chris Wirth <cwirth9@gmail.com>
2022-04-12 16:37:30 -0600

    Update Django settings for staging urls.

--- django_project/settings.py
+++ django_project/settings.py
@@ -86,6 +86,9 @@ CORS_ALLOWED_ORIGINS = (
     "https://mylifts.app",
     "https://www.mylifts.app",
     "https://mylifts.herokuapp.com",
+    "https://mylifts.netlify.app"
+    "https://mylifts-staging.herokuapp.com",
+    "https://mylifts-staging.netlify.app"
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:8000",
@@ -97,6 +100,9 @@ CSRF_TRUSTED_ORIGINS = [
     "https://mylifts.app",
     "https://www.mylifts.app",
     "https://mylifts.herokuapp.com",
+    "https://mylifts.netlify.app"
+    "https://mylifts-staging.herokuapp.com",
+    "https://mylifts-staging.netlify.app"
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
